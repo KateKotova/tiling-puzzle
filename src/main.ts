8b7452208969fe246cd4e9b9fd3c700bc87a5ab2
Kate Kotova <katekotova_86@mail.ru>
2026-01-21 13:49:13 +0300

    Add the test image; replace IIFE with promise

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,22 @@
 import { Application, Assets, Sprite } from "pixi.js";
 
-(async () => {
-  // Create a new application
-  const app = new Application();
+async function main(): Promise<void> {
+  try {
+    const app = new Application();
+    await app.init({ background: "#1099bb", resizeTo: window });
+    document.getElementById("pixi-container")!.appendChild(app.canvas);
+    const texture = await Assets.load("/assets/bunny.png");
+    const bunny = new Sprite(texture);
+    bunny.anchor.set(0.5);
+    bunny.position.set(app.screen.width / 2, app.screen.height / 2);
+    app.stage.addChild(bunny);
 
-  // Initialize the application
-  await app.init({ background: "#1099bb", resizeTo: window });
+    app.ticker.add((time) => {
+      bunny.rotation += 0.1 * time.deltaTime;
+    });
+  } catch (error) {
+    console.error(`Failed to start application: ${error}`);
+  }
+}
 
-  // Append the application canvas to the document body
-  document.getElementById("pixi-container")!.appendChild(app.canvas);
-
-  // Load the bunny texture
-  const texture = await Assets.load("/assets/bunny.png");
-
-  // Create a bunny Sprite
-  const bunny = new Sprite(texture);
-
-  // Center the sprite's anchor point
-  bunny.anchor.set(0.5);
-
-  // Move the sprite to the center of the screen
-  bunny.position.set(app.screen.width / 2, app.screen.height / 2);
-
-  // Add the bunny to the stage
-  app.stage.addChild(bunny);
-
-  // Listen for animate update
-  app.ticker.add((time) => {
-    // Just for fun, let's rotate mr rabbit a little.
-    // * Delta is 1 if running at 100% performance *
-    // * Creates frame-independent transformation *
-    bunny.rotation += 0.1 * time.deltaTime;
-  });
-})();
+main();
