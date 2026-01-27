c236e5923884d91ce1bb45db3aab2114709b8fda
Kate Kotova <katekotova_86@mail.ru>
2026-01-27 09:57:14 +0300

    T-0005 Add test

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import {
   Application,
   Assets,
   Container,
-  Graphics
+  Graphics,
+  GraphicsPath,
+  Matrix,
+  Sprite
 } from "pixi.js";
 import { TilingTextureModel } from "./models/TilingTextureModel.ts";
 import { ImageContainerModel } from "./models/ImageContainerModel.ts";
@@ -111,8 +114,53 @@ async function main(): Promise<void> {
     imageContainer.addChild(image);
 
     const tilingView = new RegularPolygonTilingView(tilingModel);
-    imageContainer.addChild(tilingView.tilingContainer);
+    //imageContainer.addChild(tilingView.tilingContainer);
     tilingView.setExampleTiling();
+
+    const path = `M14.1,121.9c-4-2.9-9.5-2-12.4,2c-1.1,1.5-1.7,3.3-1.7,5.2c0,0,0,51.2,0,51.2h51.2c1.9,0,3.7,0.6,5.2,1.7
+        c4,2.9,4.8,8.4,2,12.4c-5,6.9-3.5,16.5,3.4,21.5c6.9,5,16.5,3.5,21.5-3.4c3.9-5.4,3.9-12.7,0-18.1c-1.1-1.5-1.7-3.3-1.7-5.2
+        c0-4.9,4-8.9,8.9-8.9c0,0,51.2,0,51.2,0v-51.2c0-4.9-4-8.9-8.9-8.9c-1.9,0-3.7,0.6-5.2,1.7c-5.4,3.9-12.7,3.9-18.1,0
+        c-6.9-5-8.4-14.6-3.4-21.5c5-6.9,14.6-8.4,21.5-3.4c4,2.9,9.5,2,12.4-2c1.1-1.5,1.7-3.3,1.7-5.2c0,0,0-51.2,0-51.2H90.5
+        c-1.9,0-3.7-0.6-5.2-1.7c-4-2.9-4.8-8.4-2-12.4c5-6.9,3.5-16.5-3.4-21.5c-6.9-5-16.5-3.5-21.5,3.4c-3.9,5.4-3.9,12.7,0,18.1
+        c1.1,1.5,1.7,3.3,1.7,5.2c0,4.9-4,8.9-8.9,8.9c0,0-51.2,0-51.2,0v51.2c0,4.9,4,8.9,8.9,8.9c1.9,0,3.7-0.6,5.2-1.7
+        c5.4-3.9,12.7-3.9,18.1,0c6.9,5,8.4,14.6,3.4,21.5C30.6,125.3,21,126.9,14.1,121.9z`;
+    const shape1 = new Graphics()
+        //  An SVG path string or an array of PathInstruction objects.
+        .path(new GraphicsPath(path))      
+        // .stroke({
+        //     color: "black",
+        //     width: 2,
+        //     alpha: 0.7
+        // })
+        .fill({
+          texture,
+          textureSpace: "local"
+      });
+    shape1.pivot.set(shape1.width / 2, shape1.height / 2);
+    shape1.scale = 0.5;
+    shape1.position.set(shape1.width / 2, shape1.height / 2); 
+    imageContainer.addChild(shape1);
+
+    const shape2 = new Graphics()
+        .path(new GraphicsPath(path))      
+        // .stroke({
+        //     color: "black",
+        //     width: 2,
+        //     alpha: 0.7
+        // })
+        .fill({
+          texture,
+          textureSpace: "local",
+          matrix: new Matrix()
+            .rotate(-Math.PI / 2)
+        });
+    shape2.pivot.set(shape2.width / 2, shape2.height / 2);
+    shape2.rotation = Math.PI / 2;
+    shape2.scale = 0.5;
+    const lockHeight = (shape2.height - shape1.width) / 2;
+    shape2.position.set(shape2.height / 2 + shape1.width - lockHeight, shape2.width / 2 + lockHeight);    
+    imageContainer.addChild(shape2);
+
 
     /*
     // Bunny
