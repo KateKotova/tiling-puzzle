d0cd9f95e1427edce3cd802a5d7a10415a8beceb
Kate Kotova <katekotova_86@mail.ru>
2026-02-11 08:13:37 +0300

    T-0010 Add tiling views

---
+++ src/_new/views/tilings/TilingView.ts
@@ -0,0 +1,54 @@
+import { Color, Container, Renderer, Ticker } from "pixi.js";
+import { TilingModel } from "../../models/tilings/TilingModel.ts";
+import { ViewSettings } from "../ViewSettings.ts";
+import { DraggingTileData } from "../tile-decorators/DraggingTileData.ts";
+
+/**
+ * Класс представления замощения
+ */
+export abstract class TilingView {
+    protected viewSettings: ViewSettings;
+    public model: TilingModel;
+    public tilingContainer: Container;
+    public staticTilesContainer: Container;
+    public draggableTilesContainer: Container;
+    protected staticTileFillColor: Color = new Color(0x00AA00);
+    protected selectedTileContainer: Container;
+    protected draggingTileData: DraggingTileData = { view: null };
+
+    constructor(viewSettings: ViewSettings, model: TilingModel) {
+        if (!model.isInitialized) {
+            throw new Error('The tiling model is not initialized');
+        }
+
+        this.viewSettings = viewSettings;
+        this.model = model;
+        this.tilingContainer = this.createTilingContainer();
+
+        this.staticTilesContainer = new Container();
+        this.tilingContainer.addChild(this.staticTilesContainer);
+
+        this.draggableTilesContainer = new Container();
+        this.tilingContainer.addChild(this.draggableTilesContainer);
+
+        this.selectedTileContainer = new Container();
+        this.tilingContainer.addChild(this.selectedTileContainer);
+    }
+
+    private createTilingContainer(): Container {
+        const rectangle = this.model.tilingContainerModel!.boundingRectangle;
+        return new Container({
+            x: rectangle.x,
+            y: rectangle.y,
+            width: rectangle.width,
+            height: rectangle.height
+        });
+    }
+
+    /**
+     * Создание тестового замощения для примера
+     * @param renderer Инструменты отображения
+     * @param ticker Инструменты контроля времени
+     */
+    public abstract setExampleTiling(renderer: Renderer, ticker: Ticker): void;
+}
--- src/views/tilings/RectangularGridTilingView.ts
+++ src/views/tilings/RectangularGridTilingView.ts
@@ -1,77 +1,77 @@
-import { Renderer, Ticker } from "pixi.js";
-import { TilingView } from "./TilingView.ts";
-import { RectangularGridTilingModel } from "../../models/tilings/RectangularGridTilingModel.ts";
-import { TilingModel } from "../../models/tilings/TilingModel.ts";
-import { TileViewFactory } from "../tiles/TileViewFactory.ts";
-import { ViewSettings } from "../ViewSettings.ts";
-import { TileViewParameters } from "../tiles/TileViewParameters.ts";
-import { StaticTileView } from "../tiles/StaticTileView.ts";
-import { DragableTileView } from "../tiles/DragableTileView.ts";
+// import { Renderer, Ticker } from "pixi.js";
+// import { TilingView } from "./TilingView.ts";
+// import { RectangularGridTilingModel } from "../../models/tilings/RectangularGridTilingModel.ts";
+// import { TilingModel } from "../../models/tilings/TilingModel.ts";
+// import { TileViewFactory } from "../tiles/TileViewFactory.ts";
+// import { ViewSettings } from "../ViewSettings.ts";
+// import { TileViewParameters } from "../tiles/TileViewParameters.ts";
+// import { StaticTileView } from "../tiles/StaticTileView.ts";
+// import { DragableTileView } from "../tiles/DragableTileView.ts";
 
-export class RectangularGridTilingView extends TilingView {
-    constructor(viewSettings: ViewSettings, model: TilingModel) {
-        if (!(model instanceof RectangularGridTilingModel)) {
-            throw new Error("The tiling model is not an instance of RectangularGridTilingModel");
-        }
-        super(viewSettings, model);
-    }
+// export class RectangularGridTilingView extends TilingView {
+//     constructor(viewSettings: ViewSettings, model: TilingModel) {
+//         // if (!(model instanceof RectangularGridTilingModel)) {
+//         //     throw new Error("The tiling model is not an instance of RectangularGridTilingModel");
+//         // }
+//         super(viewSettings, model);
+//     }
 
-    public setExampleTiling(renderer: Renderer, ticker: Ticker): void {
-        const model = this.model as RectangularGridTilingModel;
-        const tileViewFactory = new TileViewFactory();
+//     public setExampleTiling(renderer: Renderer, ticker: Ticker): void {
+//         // const model = this.model as RectangularGridTilingModel;
+//         // const tileViewFactory = new TileViewFactory();
         
-        for (let rowIndex = 0; rowIndex < model.textureTileRowCount; rowIndex++) {
-            for (let columnIndex = 0; columnIndex < model.textureTileColumnCount;
-                columnIndex++) {
+//         // for (let rowIndex = 0; rowIndex < model.textureTileRowCount; rowIndex++) {
+//         //     for (let columnIndex = 0; columnIndex < model.textureTileColumnCount;
+//         //         columnIndex++) {
 
-                if (!model.getGridIndicesAreCorrect(rowIndex, columnIndex)) {
-                    continue;
-                }
+//         //         if (!model.getGridIndicesAreCorrect(rowIndex, columnIndex)) {
+//         //             continue;
+//         //         }
 
-                const filledTileModel = model.getTileModel(rowIndex, columnIndex);
-                if (!filledTileModel) {
-                    continue;
-                }
-                filledTileModel.currentRotationAngle = filledTileModel.rotationAngle;
-                filledTileModel.currentPositionPoint = filledTileModel.positionPoint.clone();
+//         //         const filledTileModel = model.getTileModel(rowIndex, columnIndex);
+//         //         if (!filledTileModel) {
+//         //             continue;
+//         //         }
+//         //         filledTileModel.currentRotationAngle = filledTileModel.rotationAngle;
+//         //         filledTileModel.currentPositionPoint = filledTileModel.positionPoint.clone();
 
-                const filledTileViewParameters: TileViewParameters = {
-                    viewSettings: this.viewSettings,
-                    model: filledTileModel,
-                    texture: null,
-                    renderer,
-                    replacingTextureFillColor: this.staticTileFillColor
-                };
-                const filledTileView = tileViewFactory.getView(filledTileViewParameters);
-                filledTileView.content.alpha = 0.7;
-                this.staticTilesContainer.addChild(filledTileView.tile);
-                const staticTileView = new StaticTileView(this.viewSettings,
-                    filledTileView, this.draggingTileData);
+//         //         const filledTileViewParameters: TileViewParameters = {
+//         //             viewSettings: this.viewSettings,
+//         //             model: filledTileModel,
+//         //             texture: null,
+//         //             renderer,
+//         //             replacingTextureFillColor: this.staticTileFillColor
+//         //         };
+//         //         const filledTileView = tileViewFactory.getView(filledTileViewParameters);
+//         //         filledTileView.content.alpha = 0.7;
+//         //         this.staticTilesContainer.addChild(filledTileView.tile);
+//         //         const staticTileView = new StaticTileView(this.viewSettings,
+//         //             filledTileView, this.draggingTileData);
 
-                const shouldCreateTexturedTile = Math.random() >= 0.5;
-                if (shouldCreateTexturedTile) {
-                    const texturedTileModel = filledTileModel.clone();
-                    texturedTileModel.currentRotationAngle = texturedTileModel.rotationAngle;
-                    texturedTileModel.currentPositionPoint
-                        = texturedTileModel.positionPoint.clone();
-                    const texturedTileViewParameters: TileViewParameters = {
-                        viewSettings: this.viewSettings,
-                        model: texturedTileModel,
-                        texture: model.getTileTexture(texturedTileModel),
-                        renderer,
-                        replacingTextureFillColor: this.staticTileFillColor
-                    };
-                    const texturedTileView = tileViewFactory.getView(texturedTileViewParameters);
-                    this.tilesContainer.addChild(texturedTileView.tile);
-                    const dragableTileView = new DragableTileView(
-                        this.viewSettings,
-                        texturedTileView,
-                        this.selectedTileContainer,
-                        ticker,
-                        this.draggingTileData);
-                    dragableTileView.dragSource = staticTileView;
-                }
-            }
-        }
-    }
-}
+//         //         const shouldCreateTexturedTile = Math.random() >= 0.5;
+//         //         if (shouldCreateTexturedTile) {
+//         //             const texturedTileModel = filledTileModel.clone();
+//         //             texturedTileModel.currentRotationAngle = texturedTileModel.rotationAngle;
+//         //             texturedTileModel.currentPositionPoint
+//         //                 = texturedTileModel.positionPoint.clone();
+//         //             const texturedTileViewParameters: TileViewParameters = {
+//         //                 viewSettings: this.viewSettings,
+//         //                 model: texturedTileModel,
+//         //                 texture: model.getTileTexture(texturedTileModel),
+//         //                 renderer,
+//         //                 replacingTextureFillColor: this.staticTileFillColor
+//         //             };
+//         //             const texturedTileView = tileViewFactory.getView(texturedTileViewParameters);
+//         //             this.tilesContainer.addChild(texturedTileView.tile);
+//         //             const dragableTileView = new DragableTileView(
+//         //                 this.viewSettings,
+//         //                 texturedTileView,
+//         //                 this.selectedTileContainer,
+//         //                 ticker,
+//         //                 this.draggingTileData);
+//         //             dragableTileView.dragSource = staticTileView;
+//         //         }
+//         //     }
+//         // }
+//     }
+// }
