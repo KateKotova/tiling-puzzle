9054fe64f0ff0d1a3cdc32da0367d5fb125347fe
Kate Kotova <katekotova_86@mail.ru>
2026-07-26 08:45:41 +0300

    T-0032 Add TriangleWithSpiralLockWithTailGeometry

--- src/models/tile-geometries/polygons-with-spiral-locks-with-tails/TriangleWithSpiralLockWithTailGeometry.ts
+++ src/models/tile-geometries/polygons-with-spiral-locks-with-tails/TriangleWithSpiralLockWithTailGeometry.ts
@@ -1,6 +1,15 @@
+import { Point } from "pixi.js";
+import { Size } from "../../../math/Size.ts";
+import { TileLockType } from "../../tile-locks/TileLockType.ts";
 import { TriangleBaseGeometry } from "../polygon-bases/TriangleBaseGeometry.ts";
+import { TileGeometryType } from "../TileGeometryType.ts";
 
 export class TriangleWithSpiralLockWithTailGeometry extends TriangleBaseGeometry {
+    public readonly geometryType: TileGeometryType
+        = TileGeometryType.TriangleWithSpiralLockWithTail;
+    public readonly lockType: TileLockType = TileLockType.SpiralWithTail;
+    
+    public svgViewBoxSize: Size = new Size(149.5, 140.5);
     public svgPath?: string =
         `M15.6,140.5c19.5,0.2,31.2-31.5,50.7-31.3c1.5,0,3.1,0.2,4.5,0.7c2.4,0.8,4.6,2.2,6.2,4.2
 c1.3,1.7,2.2,3.7,2.4,5.8c0.1,1.7-0.2,3.5-1.2,5c-0.7,1.2-1.8,2.1-3.2,2.5c-1.1,0.3-2.3,0.3-3.1-0.3c-0.6-0.5-0.9-1.3-1.2-2.2
@@ -14,4 +23,72 @@ c-0.8,1.3-1.7,2.5-2.9,3.6c-1.9,1.7-4.2,2.9-6.7,3.3c-2.1,0.3-4.3,0.1-6.2-0.8c-1.6
 c0.3-1.1,0.9-2.1,1.8-2.5c0.7-0.3,1.6-0.1,2.5,0c0.9,0.1,1.8,0.3,2.5,0c0.9-0.4,1.6-1.4,1.8-2.5c0.4-1.4,0.1-2.8-0.6-4
 c-0.8-1.5-2.2-2.7-3.7-3.5c-1.9-0.9-4.1-1.2-6.2-0.8c-2.5,0.4-4.9,1.6-6.7,3.3c-1.1,1-2.1,2.3-2.9,3.6c-10,16.8,11.7,42.7,1.7,59.5
 c-4.3,7.5-13.9,10-21.3,5.7C0,133.5,7,140.5,15.6,140.5z`;
+
+    /**
+     * Отношение высоты замка у верхней вершины треугольника
+     * в положении по умолчанию к базовой величине
+     */
+    public static readonly defaultTopVertexLockHeightToBaseValueRatio: number
+        = 0.73863566 / 50.0;
+    /**
+     * Отношение высоты замка у правой вершины треугольника
+     * в положении по умолчанию к базовой величине
+     */
+    public static readonly defaultRightVertexLockHeightToBaseValueRatio: number
+        = 0.75662582 / 50.0;
+    /**
+     * Отношение высоты замка у левой вершины треугольника
+     * в положении по умолчанию к базовой величине
+     */
+    public static readonly defaultLeftVertexLockHeightToBaseValueRatio: number = 0;
+
+    public defaultTopVertexLockHeight: number = 0;
+    public defaultRightVertexLockHeight: number = 0;
+    public defaultLeftVertexLockHeight: number = 0;
+
+    /**
+     * Отношение радиуса описанной окружности треугольника c замками включительно
+     * к базовой величине
+     */
+    private static readonly circumscribedCircleRadiusWithLocksToBaseValueRatio: number
+        = 29.76333814 / 50.0;
+
+    constructor(
+        baseValue: number,
+        sideToBaseValueRatio: number = 1,
+        hitAreaSizeMultiplier: number = 1
+    ) {
+        super(baseValue, sideToBaseValueRatio);
+
+        this.freedomDegree = this.sideCount;
+        this.freedomDegreeRotationAngle = this.getFreedomDegreeRotationAngle();
+
+        this.setLockHeight();
+
+        const sideHalf = this.side / 2.0;
+        this.pivotPoint = new Point(sideHalf + this.defaultLeftVertexLockHeight,
+            this.circumscribedCircleRadius + this.defaultTopVertexLockHeight);
+        this.defaultBoundingRectangleSize = new Size(
+            this.side + this.defaultLeftVertexLockHeight + this.defaultRightVertexLockHeight,
+            this.height + this.defaultTopVertexLockHeight + this.lockHeight
+        );
+        this.hitArea = this.getHitAreaRegularPolygon(hitAreaSizeMultiplier);
+        this.maxBoundingSize = TriangleWithSpiralLockWithTailGeometry
+            .circumscribedCircleRadiusWithLocksToBaseValueRatio
+            * this.baseValue * 2;
+    }
+
+    protected setLockHeight(): void {
+        super.setLockHeight();
+
+        this.defaultTopVertexLockHeight = this.baseValue
+            * TriangleWithSpiralLockWithTailGeometry
+            .defaultTopVertexLockHeightToBaseValueRatio;
+        this.defaultRightVertexLockHeight = this.baseValue
+            * TriangleWithSpiralLockWithTailGeometry
+            .defaultRightVertexLockHeightToBaseValueRatio;
+        this.defaultLeftVertexLockHeight = this.baseValue
+            * TriangleWithSpiralLockWithTailGeometry
+            .defaultLeftVertexLockHeightToBaseValueRatio;
+    }
 }
